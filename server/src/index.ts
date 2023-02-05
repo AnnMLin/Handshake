e32a3155a27a4f69c86c547952e6ab3e890d80bb
Ann Lin <amlin.tuo@gmail.com>
2023-02-05 14:57:25 -0500

    Add POST /experiments endpoint
    Add POST /experiments endpoint to handle saving experiment
    activation data to experiments table in DB. Create experiments
    table if table does not exist in DB.

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,16 @@ import sqlite3 from "sqlite3";
 
 const app = express();
 const db = new sqlite3.Database("./analytics.sqlite");
+db.serialize(() => {
+  db.run(
+    "CREATE TABLE IF NOT EXISTS experiments( user_id INTEGER NOT NULL, experiment_name TEXT NOT NULL, experiment_group TEXT NOT NULL, experiment_start_dt TEXT NOT NULL)",
+    (err) => {
+      if (err) {
+        throw err;
+      }
+    }
+  );
+});
 const corsOpt: cors.CorsOptions = {
   origin: ["http://localhost:3000"],
 };
@@ -20,6 +30,39 @@ app.post("/events", async (req: Request, res: Response) => {
     [name]
   );
   return res.json({ success: true });
+});
+
+app.post("/experiments", async (req: Request, res: Response) => {
+  const { userId, experimentName } = req.body;
+
+  await db.get(
+    "SELECT experiment_group FROM experiments WHERE experiment_name = ? AND user_id = ?",
+    [experimentName, userId],
+    async (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      const experimentGroup = data ? data.experiment_group : null;
+
+      if (experimentGroup) {
+        return res.json({ experimentGroup });
+      } else {
+        const experimentGroup = userId % 2 ? "enabled" : "control";
+        await db.run(
+          "INSERT INTO experiments(user_id, experiment_name, experiment_group, experiment_start_dt) VALUES ( ?, ?, ?, datetime('now'))",
+          [userId, experimentName, experimentGroup],
+          (err) => {
+            if (err) {
+              throw err;
+            }
+          }
+        );
+
+        return res.json({ experimentGroup });
+      }
+    }
+  );
 });
 
 app.listen({ port: 8081 });
