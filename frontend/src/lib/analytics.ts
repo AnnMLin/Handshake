0f81935886777e43a28c1cf5390256c2727c7958
Ann Lin <amlin.tuo@gmail.com>
2023-02-06 12:34:33 -0500

    Add log method to Analytics class
    Add log method to exisiting Analytics class to make POST calls to
    POST /loggings endpoint. Add unit test for log method.

--- frontend/src/lib/analytics.ts
+++ frontend/src/lib/analytics.ts
@@ -1,10 +1,20 @@
-import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:8081';
+import { getContext } from "./context";
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:8081";
 
 class Analytics {
   async track(name: string) {
     await axios.post('/events', {name});
   }
-};
+
+  async log(component: string, action: string) {
+    const userId = getContext().userId;
+    await axios.post("/loggings", {
+      userId,
+      component,
+      action,
+    });
+  }
+}
 
 export default new Analytics();
---
+++ frontend/src/lib/test/analytics.test.ts
@@ -0,0 +1,21 @@
+import Analytics from "../analytics";
+import axios from "axios";
+
+jest.mock("axios");
+
+describe("Analytics class", () => {
+  describe("log method", () => {
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it("makes axios POST calls", async () => {
+      const post = jest
+        .spyOn(axios, "post")
+        .mockResolvedValue({ data: { success: true } });
+      await Analytics.log("BUTTON", "CLICK");
+
+      expect(post).toHaveBeenCalled();
+    });
+  });
+});
